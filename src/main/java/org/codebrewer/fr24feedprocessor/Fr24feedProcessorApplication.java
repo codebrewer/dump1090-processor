a9d734b5c757c89a7ab3b87e37be42b36e4a0558
Mark Scott <mark@codebrewer.org>
2017-12-09 19:33:57 +0000

    Spring Initializr bootstrap project

---
+++ src/main/java/org/codebrewer/fr24feedprocessor/Fr24feedProcessorApplication.java
@@ -0,0 +1,12 @@
+package org.codebrewer.fr24feedprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fr24feedProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fr24feedProcessorApplication.class, args);
+	}
+}
