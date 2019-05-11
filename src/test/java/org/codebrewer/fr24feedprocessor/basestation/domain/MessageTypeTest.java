4841620d85fc5218f82d3ce0e322721e28444aec
Mark Scott <mark@codebrewer.org>
2019-05-11 20:05:48 +0100

    Add unit tests for MessageType enum
    Only adding tests for one class to see the impact on Codecov reports.
    
    #1 Consume and optionally persist SBS-1 BaseStation messages

---
+++ src/test/java/org/codebrewer/fr24feedprocessor/basestation/domain/MessageTypeTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 Mark Scott
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codebrewer.fr24feedprocessor.basestation.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MessageTypeTest {
+
+  private static void assertMessageTokenCount(MessageType messageType, int expectedTokenCount) {
+    Assertions.assertThat(messageType.getMessageTokenCount()).isEqualTo(expectedTokenCount);
+  }
+
+  @Test
+  void shouldReturnCorrectMessageTokenCountForMessageTypeAIR() {
+    assertMessageTokenCount(MessageType.AIR, 10);
+  }
+
+  @Test
+  void shouldReturnCorrectMessageTokenCountForMessageTypeID() {
+    assertMessageTokenCount(MessageType.ID, 11);
+  }
+
+  @Test
+  void shouldReturnCorrectMessageTokenCountForMessageTypeMSG() {
+    assertMessageTokenCount(MessageType.MSG, 22);
+  }
+
+  @Test
+  void shouldReturnCorrectMessageTokenCountForMessageTypeSTA() {
+    assertMessageTokenCount(MessageType.STA, 11);
+  }
+}
